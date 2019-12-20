fd8899b0029b537730d598b61b6f82f143d78bb2
giulia <giuliafazzi@hotmail.com>
2019-12-20 04:38:03 -0300

    navegação

--- ecoponto/app/urls.py
+++ ecoponto/app/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from app.views import IndexView
+from app.views import IndexView, EcopontosView, SobreView, EmpresaView, DuvidasView, ContatoView
 
 app_name = 'app'
 
 urlpatterns = [
-    path('', IndexView, name='index')
+    path('', IndexView, name='index'),
+    path('ecopontos/', EcopontosView, name='ecopontos'),
+    path('sobre/', SobreView, name='sobre'),
+    path('empresa/', EmpresaView, name='empresa'),
+    path('duvidas/', DuvidasView, name='duvidas'),
+    path('contato/', ContatoView, name='contato')
 ]
