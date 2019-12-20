fad6a499efde35810dd686a36ec1d350a9e89e95
Felipe Guimarães <luiz.guimaraes@dcomp.sor.ufscar.br>
2019-12-20 02:37:20 -0300

    fixed view

--- ecoponto/app/views.py
+++ ecoponto/app/views.py
@@ -3,6 +3,13 @@ from app.models import Empresa, Material, Certificado
 
 def IndexView(request):
     empresas = Empresa.objects.all()
-    context = {'empresas': empresas}
+    materiais = Material.objects.all()
+    certificados = Certificado.objects.all()
+    
+    context = {
+        'empresas': empresas,
+        'materiais': materiais,
+        'certificados': certificados,
+    }
     return render(request=request, template_name='app/index.html', context=context)
 
